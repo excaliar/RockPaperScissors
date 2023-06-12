8a8303bb17ad0f85e8c40ef9fa862bd97982045d
excaliar <langanmatt55@gmail.com>
2023-06-12 14:21:17 -0400

    create function to update game score in display and print winner (first to reach 3)

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+//scores for player and computer (resets upon reaching 3)
+let p = 0
+let c = 0
 
+function checkWinner(){
+    if (p == 3 || c == 3) {
+        if (p == 3) {
+            p = 0;
+            c = 0;
+            return setTimeout(() => {roundResult.textContent = "Player Wins!"; }, 1000);
+        }
+        if (c == 3) {
+            p = 0;
+            c = 0;
+            return setTimeout(() => {roundResult.textContent = "Computer Wins!"; }, 1000);
+        }
+    }
+}
 
 function getComputerChoice() {
-    choice = ["Rock", "Paper", "Scissor"]
+    const choice = ["Rock", "Paper", "Scissor"]
     
-    min = Math.ceil(0);
-    max = Math.floor(2);
-    random = Math.floor(Math.random() * (max-min + 1) + min);
+    const min = Math.ceil(0);
+    const max = Math.floor(2);
+    let random = Math.floor(Math.random() * (max-min + 1) + min);
 
     return choice[random];
 }
@@ -17,8 +34,10 @@ function oneRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.replace(computerSelection[0], computerSelection[0].toUpperCase())
 
     if ((playerSelection == 'Rock' && computerSelection == 'Scissor') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissor' && computerSelection == 'Paper')) {
+        p += 1;
         return(`You Win! ${playerSelection} beats ${computerSelection}`);
     } else if ((playerSelection == 'Scissor' && computerSelection == 'Rock') || (playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissor')) {
+        c += 1;
         return(`You Lose! ${computerSelection} beats ${playerSelection}`);
     } else {
         return("It's a Tie!");
@@ -27,6 +46,8 @@ function oneRound(playerSelection, computerSelection) {
 
 const display = document.querySelector('.display');
 const roundResult = document.createElement('div');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper')
@@ -34,15 +55,25 @@ const scissors = document.querySelector('#scissors')
 
 rock.addEventListener('click', () => {
     roundResult.textContent = (oneRound('rock', getComputerChoice()))
+    player.textContent = `Player: ${p}`
+    computer.textContent = `Computer: ${c}`
+    checkWinner()
 })
 
 paper.addEventListener('click', () => {
     roundResult.textContent = (oneRound('paper', getComputerChoice()))
+    player.textContent = `Player: ${p}`
+    computer.textContent = `Computer: ${c}`
+    checkWinner()
 })
 
 scissors.addEventListener('click', () => {
     roundResult.textContent = (oneRound('scissor', getComputerChoice()))
+    player.textContent = `Player: ${p}`
+    computer.textContent = `Computer: ${c}`
+    checkWinner()
 })
 
 display.appendChild(roundResult);
+
 
